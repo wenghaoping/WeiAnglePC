1ced51b78f4c084598706d870977f94177ab6977
wenghaoping <wenghaoping@sina.com>
2018-01-12 14:23:41 +0800

    手机端入口

--- src/filters/index.js
+++ src/filters/index.js
@@ -21,10 +21,58 @@ Vue.filter('timeToReallTime', (value) => {
   // second = second < 10 ? ('0' + second) : second;
   return y + '-' + m + '-' + d;/* +' '+h+':'+minute+':'+second; */
 });
+Vue.filter('timeToReallTime_pointType', (value) => {
+  if (value === '' || value === undefined || value === null) return '';
+  var date = new Date();
+  if (value.length > 11) date.setTime(value);
+  else date.setTime(value * 1000);
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  m = m < 10 ? ('0' + m) : m;
+  // var h = date.getHours();
+  // h = h < 10 ? ('0' + h) : h;
+  // var minute = date.getMinutes();
+  // var second = date.getSeconds();
+  // minute = minute < 10 ? ('0' + minute) : minute;
+  // second = second < 10 ? ('0' + second) : second;
+  return y + '.' + m;/* +' '+h+':'+minute+':'+second; */
+});
+Vue.filter('timeToReallTime_lineTopoint', (value) => {
+  if (value) {
+    return value.substr(0, 4) + '.' + value.substr(5, 2);
+  }
+});
+
+Vue.filter('timeToReallTimeAll', (value) => {
+  if (value === '' || value === undefined || value === null) return '';
+  var date = new Date();
+  if (value.length > 11) date.setTime(value);
+  else date.setTime(value * 1000);
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  m = m < 10 ? ('0' + m) : m;
+  var d = date.getDate();
+  d = d < 10 ? ('0' + d) : d;
+  var h = date.getHours();
+  h = h < 10 ? ('0' + h) : h;
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  minute = minute < 10 ? ('0' + minute) : minute;
+  second = second < 10 ? ('0' + second) : second;
+  return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
+});
 
 Vue.filter('nullToZ', (value) => {
   if (value === '' || value === undefined || value === null) {
     return '暂无数据';
+  } else {
+    return value;
+  }
+});
+
+Vue.filter('nullToB', (value) => {
+  if (value === '' || value === undefined || value === null) {
+    return '暂未编辑';
   } else {
     return value;
   }
---
+++ src/views/mobileEntranceIndex/getContact.vue
@@ -0,0 +1,55 @@
+<template>
+  <div  id='getContact'>
+    <el-dialog
+      title="获取联系方式"
+      :visible.sync="dialogVisible"
+      :before-close="handleClose"
+      width = '90'
+      center = true>
+      <el-input type="textarea" name="contactText" v-model='contactText' :cols=30 :rows = 3 resize='none' placeholder="请简要描述您意向的约谈时间、地点、方式"></el-input>
+      <div class="text" style="margin-top: 1rem;">微天使会在第一时间告诉你联系方式</div>
+      <div class="text">或添加FA哥微信：weitianshicn</div>
+      <span slot="footer" class="dialog-footer">
+      <el-button class="certain" type="primary" style="background:#40587a;border-radius:3px;" @click = 'handleClose(1)'>确 定</el-button>
+    </span>
+    </el-dialog>
+  </div>
+</template>
+
+
+<script type="text/ecmascript-6">
+  export default {
+    props: [
+      'dialogVisible', 'projectId'
+    ],
+    data () {
+      return {
+        contactText: '',
+        project_id: ''
+      };
+    },
+    methods: {
+      handleClose (i) {
+        if (i === 1) {
+          this.$emit('closeGetContact', this.contactText);
+        } else {
+          this.$emit('closeGetContact', 'close');
+        }
+      }
+    },
+    mounted () {
+    }
+  };
+</script>
+
+<style  lang="less">
+  @import '../../assets/css/mobileEntrance.less';
+  #getContact{
+    .el-dialog--small {
+      width: 75%;
+    }
+    .text{
+      text-align: center;
+    }
+  }
+</style>
