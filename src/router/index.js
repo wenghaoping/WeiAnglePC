f694f076a0d6cc1e9fabfd9dadcc526fef7ab2b3
wenghaoping <541920112@qq.com>
2018-03-26 10:24:50 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,6 @@ export default new Router({
         {path: 'myActivity', component: _import('Activity/myActivity'), name: 'myActivity'}
       ]
     },
-    // {path: 'myActivity', component: _import('Activity/myActivity'), name: 'myActivity'},
     {
       path: '*', redirect: '/' // 默认回到首页
     },
@@ -101,9 +100,6 @@ export default new Router({
     },
     {
       path: '/creatActivity', name: 'creatActivity', component: _import('Activity/creatActivity') // 创建活动
-    },
-    {
-      path: '/myActivity', name: 'myActivity', component: _import('Activity/myActivity') // 我的活动
     },
     {
       path: '/successActivity', name: 'successActivity', component: _import('Activity/successActivity') // 我的活动
